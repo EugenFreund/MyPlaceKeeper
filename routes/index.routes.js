ccee4df8731fc7c589ed2e97f91cdfa91ca44b85
Eugen Freund <Eugen.Freund@outlook.de>
2023-10-24 10:33:29 +0200

    add User schema and auth routes with JWT

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
+const authRoutes = require('./auth.routes')
+router.use('/auth', authRoutes)
 
 module.exports = router;
