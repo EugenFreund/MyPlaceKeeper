1417928485070e61301519fceb286e7d628c5a26
Eugen Freund <Eugen.Freund@outlook.de>
2023-10-24 15:22:27 +0200

    add Pin schema

--- models/User.model.js
+++ models/User.model.js
@@ -10,6 +10,13 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true
     },
+    email: {
+      type: String,
+      required: [true, 'Email is required.'],
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
     passwordHash: {
       type: String,
       required: [true, 'Password is required.']
